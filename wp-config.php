2c351d7ae0020b31cf0f061ca6ed08a83a350dfa
khaledhossam91 <130113545+khaledhossam91@users.noreply.github.com>
2023-04-07 20:30:50 +0200

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,4 @@
+define( 'WPSCAN_API_TOKEN', '$your_api_token' );
+define( 'PANTHEON_WPSCAN_ENVIRONMENTS', 'live' );
+define( 'PANTHEON_WPSCAN_ENVIRONMENTS', ['dev', 'test', 'live'] );
+define( 'PANTHEON_WPSCAN_ENVIRONMENTS', '*' );
